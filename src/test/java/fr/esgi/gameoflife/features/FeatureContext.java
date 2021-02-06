949ed5172a6799bec4d588c858d33b0b02173869
Antonin Texier <38111887+chukitipok@users.noreply.github.com>
2021-02-06 19:50:56 +0100

    Delete FeatureContext.java

--- src/test/java/fr/esgi/gameoflife/features/FeatureContext.java
+++
@@ -1,9 +0,0 @@
-package fr.esgi.gameoflife.features;
-
-import fr.esgi.gameoflife.model.Game;
-import fr.esgi.gameoflife.model.Grid;
-
-public class FeatureContext {
-
-    // private Game game = new Game(new Grid());
-}
